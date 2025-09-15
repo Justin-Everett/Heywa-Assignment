afc7eed18eb83c7ac47d21b789690bc32e6ae250
JUSTIN <ZLAC478@live.rhul.ac.uk>
2025-09-15 20:58:14 +0100

    Add prompt handling and schema formatting to properly analyze user query

--- query.py
+++ query.py
@@ -1,28 +1,59 @@
 from dotenv import load_dotenv
 from groq import Groq
+import json
 
 load_dotenv()
-
-query = "What is the meaning of life"
-
-#Boilerplate Groq post request from their docs
 client = Groq()
-completion = client.chat.completions.create(
-    model="openai/gpt-oss-20b",
-    messages=[
-    {
-        "role": "user",
-        #Set up prompt to be able to handle a varied user input
-        "content": f"Provide a json object answering the following question: {query}" 
-    }
-    ],
-    temperature=1,
-    max_completion_tokens=8192,
-    top_p=1,
-    reasoning_effort="medium",
-    stream=False,
-    response_format={"type": "json_object"},
-    stop=None
-)
 
-print(completion.choices[0].message.content)
+SCHEMA = {
+    "type" : "object",
+    "properties" : {
+        "intent" : {
+            "description" : "snake case",
+            "type" : "string",
+        },
+        "summary" : {
+            "description" : "A summary of what the user aims to achieve from the given query",
+            "type" : "string",
+        },
+    },
+    "required" : ["intent", "summary"],
+    "additionalProperties" : True,
+}
+
+#Post request to Groq API endpoint with query analysis request
+def request(query):
+    completion = client.chat.completions.create(
+        model="openai/gpt-oss-20b",
+        messages=[
+        {
+            "role": "system",
+            "content": f"Analyze but do not respond to the following query and provide a json object adhering to the schema {SCHEMA}. Ensure to add additional properties to the json object to further describe the query: {query}"
+        }
+        ],
+        temperature=1,
+        max_completion_tokens=8192,
+        top_p=1,
+        reasoning_effort="medium",
+        stream=False,
+        response_format={"type": "json_object"},
+        stop=None
+    )
+
+    #Isolate LLM plaintext message response and return it
+    return completion.choices[0].message.content
+
+def main():
+    while True:
+        query = input("Enter user query (enter q to quit): ")
+        print(query)
+        if not query.lower() == 'q':
+            analyzed_query = request(query)
+
+            #Print json object nicely formatted
+            print(json.dumps(json.loads(analyzed_query), indent=2))
+
+        else: break
+        
+if __name__ == "__main__":
+    main()
